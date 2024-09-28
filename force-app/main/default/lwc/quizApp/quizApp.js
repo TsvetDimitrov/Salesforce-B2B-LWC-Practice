1ef15328574fad0ca12088eb25e19d6fce593785
Tsvetomir Dimitrov <dimitrovv32@gmail.com>
2024-09-28 20:31:28 +0300

    Create quizApp using LWC.

---
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -0,0 +1,59 @@
+import { LightningElement } from 'lwc';
+
+export default class QuizApp extends LightningElement {
+
+    selected = {}; // storing answers.
+
+    myQuestions = [
+        {   
+            id: "Question1",
+            question: "Which of the following is not a template loop?",
+            answers: {
+                a: "for:each",
+                b: "iterator",
+                c: "map loop"
+            },
+            correctAnswer: "c"
+        },
+        {   
+            id: "Question2",
+            question: "Which file is invalid in LWC?",
+            answers: {
+                a: ".svg",
+                b: ".apex",
+                c: ".css"
+            },
+            correctAnswer: "b"
+        },
+        {   
+            id: "Question3",
+            question: "Which of the following is not a directive?",
+            answers: {
+                a: "for:each",
+                b: "if:true",
+                c: "@track"
+            },
+            correctAnswer: "c"
+        }
+    ];
+
+    get allNotSelected () {
+        return !(Object.keys(this.selected).length == this.myQuestions.length)
+    }
+
+    changeHandler (event) {
+        console.log("name", event.target.name);
+        console.log("value", event.target.value);
+
+        const {name, value} = event.target;
+        this.selected = {...this.selected, [name]: value}
+    }
+
+    submitHandler (e) {
+
+    }
+
+    changeHandler (e) {
+        
+    }
+}
